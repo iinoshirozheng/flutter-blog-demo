830def645eb13cde70d74f3c48bf834b07fdaa34
ray <iinoshirozheng@gmail.com>
2024-04-03 01:29:41 +0800

    add Appbar : mouse region

--- lib/components/tabs_button.dart
+++ lib/components/tabs_button.dart
@@ -11,11 +11,29 @@ class TabsButton extends StatefulWidget {
 }
 
 class _TabsButtonState extends State<TabsButton> {
+  bool isSelected = false;
+
   @override
   Widget build(BuildContext context) {
-    return Text(
-      widget.title,
-      style: GoogleFonts.oswald(color: Colors.black, fontSize: 23.0),
+    return MouseRegion(
+      onEnter: (_) => setState(() => isSelected = true),
+      onExit: (_) => setState(() => isSelected = false),
+      child: AnimatedDefaultTextStyle(
+        duration: const Duration(milliseconds: 100),
+        curve: Curves.elasticIn,
+        style: SelectTextStyle(isSelected),
+        child: Text(widget.title),
+      ),
     );
   }
+
+  TextStyle SelectTextStyle(bool selected) => selected
+      ? GoogleFonts.oswald(
+          shadows: [Shadow(color: Colors.black, offset: Offset(0, -5))],
+          color: Colors.transparent,
+          fontSize: 25.0,
+          decoration: TextDecoration.underline,
+          decorationThickness: 2,
+          decorationColor: Colors.tealAccent)
+      : GoogleFonts.oswald(color: Colors.black, fontSize: 23.0);
 }
--- lib/web/web_landing_page.dart
+++ lib/web/web_landing_page.dart
@@ -19,7 +19,17 @@ class _WebLandingPageState extends State<WebLandingPage> {
         elevation: 0.0,
         title: Row(
           children: [
+            Spacer(flex: 1),
             TabsButton(title: "Home"),
+            Spacer(flex: 2),
+            TabsButton(title: "Works"),
+            Spacer(flex: 2),
+            TabsButton(title: "Blog"),
+            Spacer(flex: 2),
+            TabsButton(title: "About"),
+            Spacer(flex: 2),
+            TabsButton(title: "Contact"),
+            Spacer(flex: 1),
           ],
         ),
       ),
